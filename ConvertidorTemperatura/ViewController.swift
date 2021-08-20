514152f57ebce5eda4181eb9bbafa523d8662ddc
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2021-08-19 19:42:45 -0700

    :lipstick: Connecting logic to UI

--- ConvertidorTemperatura/ViewController.swift
+++ ConvertidorTemperatura/ViewController.swift
@@ -13,12 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var celsiusTextField: UITextField!
     @IBOutlet weak var fahrenheitTextField: UITextField!
     
+    let temperatureConverter = TemperatureConverter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func convert(_ sender: UIButton) {
+        fahrenheitTextField.text = ""
+                //! al final en Float16(celcius) significa que pasa lo que pase se sabe que va a llegar un numero
+                if let celciusValue = celsiusTextField.text {
+                    if !celciusValue.isEmpty {
+                        let fahrenheitValue = temperatureConverter.convert(temperature: Temperature(value: Float16(celciusValue)!, unit: Temperature.Unit.CELSIUS), unitToConvert: Temperature.Unit.FAHRENHEIT)
+                        print("Farenheit " + String(fahrenheitValue.value))
+                        fahrenheitTextField.text = String(fahrenheitValue.value)
+                    } else {
+                        print("Celcius value is empty")
+                    }
+                }
     }
     
     
